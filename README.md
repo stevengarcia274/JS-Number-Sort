a6ac97c429b6be2cf8931792848fbc4897920ab0
Steven Garcia <98284548+stevengarcia274@users.noreply.github.com>
2023-05-05 12:33:04 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+JS number sort visualization.
+<br>
+<br>
+(5/5/23) V.1 is just attempt at visualizing a numeric array and sorting it at the click of a button. A future version might include more CSS styles, and a breakdown of the step by step sort process. My method of sorting for this push was using .sort() along with a comparison function. I included a hidden script of a different (perhaps less effective) way of sorting. I might use this in the future if I think it'll help with the visualization aspect since it allows me to see more of what goes on behind the scenes.
---
+++ numSort.js
@@ -0,0 +1,41 @@
+function visualizeArr(inputString){
+   const inputToArr = inputString.split(" ").filter(Boolean);
+   /* learn more about how this filter works and how we did it */
+
+   if (inputToArr.length > 10){
+      window.alert("ONLY 10 ITEMS ALLOWED IN ARRAY.\nTRY AGAIN.");
+   }
+
+   else{
+      document.getElementById("insert-here").innerHTML = arrItemsDisplay(inputToArr);
+   }
+}
+
+function arrItemsDisplay(inputArr){
+   let htmlInsert = ``;
+   for(let i = 0; i < inputArr.length; i++){
+      htmlInsert += `<div style="border:2px solid black; padding: 12px;">${inputArr[i]}</div>`;
+   }
+
+   return htmlInsert;
+}
+
+function clearInput(){
+   document.getElementById("text-demo1").value = "";
+   document.getElementById("insert-here").innerHTML = "";
+}
+
+function arrSort(inputToSrt){
+   let sortedArr = inputToSrt.split(" ").filter(Boolean);
+   document.getElementById("insert-here").innerHTML = arrItemsDisplay(sortedArr.sort(function (a, b){
+      if (a < b){
+         return -1;
+      }
+      if (b < a){
+         return 1;
+      }
+      return 0;
+   }));
+}
+
+
